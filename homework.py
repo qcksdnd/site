58449cd5280ead2672d1413032e491d5e81a9847
qcksdnd <24-30421@daegu-hwawon.hs.kr>
2024-05-23 19:30:39 +0900

    Add files via upload

--- homework.py
+++ homework.py
@@ -9,12 +9,11 @@ with tab1:
     # tab A 를 누르면 표시될 내용
     st.write("")
     st.subheader('1. 간단한 자기소개')
-    st.markdown("이름은 **박찬웅**이고, 5/23 현재 시각 6시 51분 자기소개중이고 2024년 기준으로 나이는 19세 **고3**이며 **수능** 약 100몇일 앞두고 있는 **수험생**이다.")
+    st.markdown("이름은 **박찬웅**, 2024년 기준으로 나이는 19세 **고3**이며 **수능**을 앞두고 있는 **수험생**이다.")
 
     st.subheader('2. 나의 역사')
-    st.markdown('경기도 용인시의 한 병원에서 태어났으며, 하늘보리 어린이집, 강남 유치원, 갈곡초등학교, 대구옥포초등학교, 달성중학교, 그리고 현재 화원고등학교까지 많은 학교를 거쳐 현재 고등학교 3학년이 되었다.')
+    st.markdown('하늘보리 어린이집, 강남 유치원, 갈곡초등학교, 대구옥포초등학교, 달성중학교, 그리고 현재 화원고등학교까지 많은 학교를 거쳐 현재 고등학교 3학년이 되었다.')
     st.markdown(':red[[**밑에 사진들은 나의 모교들**]]')
-    st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.naver.com%2Fsunju14%2F220887352522%3FviewType%3Dpc&psig=AOvVaw3BAzwtAQUa7w4Li6RZaGGc&ust=1716545135265000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCMj284vDo4YDFQAAAAAdAAAAABAQ')
     st.image('https://yongin.grandculture.net/Image?localName=yongin&id=GC009P0182&t=middle')
 
     st.caption('출처: 내 머릿속')
